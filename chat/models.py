0e1d13dfe218944f10e92afbc67300034e192473
Alexej Hein <alexej.hein@aol.de>
2024-03-29 19:44:09 +0100

    commit

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,13 @@
+from datetime import date
+from django.conf import settings
+from django.db.models.fields import DateField
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    created_at = DateField(default=date.today)
+    # chat = Chat class
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_messages_set')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                 related_name='receiver_messages_set')
