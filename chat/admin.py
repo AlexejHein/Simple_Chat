9b338b985ef4e0b371ece5ae58ceea2ebdb15ab5
Alexej Hein <alexej.hein@aol.de>
2024-03-30 16:05:23 +0100

    commit

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from chat.models import Message
 
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ['text', 'created_at', 'author', 'receiver']
+    list_display = ['text', 'created_at', 'author', 'receiver']
+    search_fields = ('text',)
+
+
+admin.site.register(Message, MessageAdmin)
