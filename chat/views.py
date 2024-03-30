6f50639ce75e8d1b440c151894d2aca8b039fecc
Alexej Hein <alexej.hein@aol.de>
2024-03-30 16:32:31 +0100

    commit

--- chat/views.py
+++ chat/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def index(request):
+    if request.method == 'POST':
+        print(request.POST.get('textmessage'))
     return render(request, 'chat/index.html', {})
