f4b318f382f9f3545ad184406e536cf3b19f3c41
Alexej Hein <alexej.hein@aol.de>
2024-03-31 17:12:09 +0200

    commit

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from .models import Message, Chat
+
 
 def index(request):
     if request.method == 'POST':
         print(request.POST.get('textmessage'))
-    return render(request, 'chat/index.html', {})
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST.get('textmessage'), chat=myChat,
+                               author=request.user, receiver=request.user)
+    chatMessages = Message.objects.all()
+    return render(request, 'chat/index.html', {'messages': chatMessages})
