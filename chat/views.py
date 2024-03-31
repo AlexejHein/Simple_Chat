49a3c305ab48a5e73cb20d3e23e95b46fc7fd07a
Alexej Hein <alexej.hein@aol.de>
2024-03-31 18:01:47 +0200

    commit

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-
 from .models import Message, Chat
 
 
+@login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
         print(request.POST.get('textmessage'))
@@ -16,11 +17,12 @@ def index(request):
 
 
 def login_view(request):
+    redirect = request.GET.get('next')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
         if user:
             login(request, user)
-            return HttpResponseRedirect('/chat/')
+            return HttpResponseRedirect(request.POST.get('redirect'))
         else:
-            return render(request, 'auth/login.html', {'wrongPassword': True})
-    return render(request, 'auth/login.html')
+            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect})
