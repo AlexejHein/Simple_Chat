2440cdd12d7c4eb11212de84c073063b34b1af52
Alexej Hein <alexej.hein@aol.de>
2024-04-02 10:26:16 +0200

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -13,3 +13,21 @@ document.querySelector('.sand-box form').addEventListener('submit', function(eve
     setTimeout(scrollToEnd, 10);
 });
 
+window.sendMessage = async function() {
+    let messageField = document.getElementById('messageField');
+    let fd = new FormData();
+    let token = '{{ csrf_token }}';
+    fd.append('textmessage', messageField.value);
+    fd.append('csrfmiddlewaretoken', token);
+    try {
+        await fetch('/chat/', {
+                method: 'POST',
+                body: fd
+            }).then(r => {
+                console.log('Message sent');
+        });
+
+    } catch (e) {
+        console.error('Error sending message');
+    }
+}
