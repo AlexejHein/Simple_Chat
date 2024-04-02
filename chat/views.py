6ee9961db503a5207fc43a2aadd1cb61ee27e6e2
Alexej Hein <alexej.hein@aol.de>
2024-04-02 10:01:19 +0200

    commit

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
@@ -29,7 +30,15 @@ def login_view(request):
 
 
 def signup_view(request):
-    return render(request, 'auth/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth/signup.html', {'form': form})
 
 
 def logout_view(request):
