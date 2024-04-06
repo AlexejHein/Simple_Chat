6fd2b9e4bfc4d343fb050cbd87fb1aae0609bdab
Alexej Hein <alexej.hein@aol.de>
2024-04-06 10:27:56 +0200

    commit

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,12 @@
 from datetime import date
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=150)
 
 
 class Chat(models.Model):
